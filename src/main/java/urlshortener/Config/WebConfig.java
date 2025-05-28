cdd7a8bea342473d5531e0473d7619382a921565
Salomão Vasques <125229303+sachow12@users.noreply.github.com>
2025-05-27 22:48:09 -0300

    allowing production origin on CORS

--- src/main/java/urlshortener/Config/WebConfig.java
+++ src/main/java/urlshortener/Config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://nowshort.up.railway.app")
+                        .allowedOrigins("http://localhost:3000", "https://nowshort.up.railway.app", "https://encurtadordeurl.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
